675be00f3e0160619376965048af30ddb68a2754
Jordan S <jpstanway@gmail.com>
2018-12-30 15:21:20 -0800

    can pass along 2 stocks which will return both objects with value showing difference in likes

--- controllers/stockHandler.js
+++ controllers/stockHandler.js
@@ -10,51 +10,55 @@ const CONNECTION_STRING = process.env.DB;
 var request = require("request");
 
 function StockHandler() {
-  this.getPrice = function(stock, callback) {
-    //var result;
-    request(`https://api.iextrading.com/1.0/stock/${stock}/price`, function(
-      error,
-      response,
-      body
-    ) {
-      if (!error && response.statusCode == 200) {
-        callback("stockData", body);
-      } else {
-        console.log("issue!");
-        callback("stockData", "external source error");
-      }
+  this.getPrice = stock => {
+    return new Promise((resolve, reject) => {
+      // get data from api
+      request(
+        `https://api.iextrading.com/1.0/stock/${stock}/price`,
+        (err, res, body) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(JSON.parse(body));
+          }
+        }
+      );
     });
   };
 
-  this.handleLikes = function(stock, like, ip, callback) {
-    MongoClient.connect(
-      CONNECTION_STRING,
-      function(err, db) {
-        var collection = db.collection("stock_likes");
-        if (!like) {
-          collection.find({ stock: stock }).toArray(function(err, doc) {
-            var likes = 0;
-            if (doc.length > 0) {
-              likes = doc[0].likes.length;
-            }
-            callback("likeData", { stock: stock, likes: likes });
-          });
-        } else {
-          collection.findAndModify(
-            { stock: stock },
-            [],
-            { $addToSet: { likes: ip } },
-            { new: true, upsert: true },
-            function(err, doc) {
-              callback("likeData", {
-                stock: stock,
-                likes: doc.value.likes.length
+  this.handleLikes = (stock, like, ip) => {
+    return new Promise((resolve, reject) => {
+      // connect to db and either add to or just retrieve like data
+      MongoClient.connect(
+        CONNECTION_STRING,
+        function(err, db) {
+          if (err) {
+            reject(err);
+          } else {
+            const collection = db.collection("stock_likes");
+            if (!like) {
+              collection.find({ stock: stock }).toArray(function(err, doc) {
+                var likes = 0;
+                if (doc.length > 0) {
+                  likes = doc[0].likes.length;
+                }
+                resolve({ stock: stock, likes: likes });
               });
+            } else {
+              collection.findAndModify(
+                { stock: stock },
+                [],
+                { $addToSet: { likes: ip } },
+                { new: true, upsert: true },
+                function(err, doc) {
+                  resolve({ stock: stock, likes: doc.value.likes.length });
+                }
+              );
             }
-          );
+          }
         }
-      }
-    );
+      );
+    });
   };
 }
 
--- routes/api.js
+++ routes/api.js
@@ -18,17 +18,66 @@ module.exports = function(app) {
 
   app.route("/api/stock-prices").get(function(req, res) {
     const stock = req.query.stock;
-    const like = req.query.like === "true" ? true : false;
+    const like = req.query.like || false;
     const ip = req.connection.remoteAddress;
-    console.log(like);
-    stockPrices.getPrice(stock, (obj, priceData) => {
-      // retrieve and/or add likes to db
-      stockPrices.handleLikes(stock, like, ip, (obj, likeData) => {
-        // return stockData object
-        res.json({
-          [obj]: { stock: stock, price: priceData, likes: likeData.likes }
-        });
-      });
-    });
+
+    if (Array.isArray(stock)) {
+      // process and compare multiple stocks
+      const stockData = [];
+      const price1 = stockPrices.getPrice(stock[0]);
+      const likes1 = stockPrices.handleLikes(stock[0]);
+      const price2 = stockPrices.getPrice(stock[1]);
+      const likes2 = stockPrices.handleLikes(stock[1]);
+
+      price1
+        .then(priceData1 => {
+          const stock1 = {};
+          stock1.stock = stock[0];
+          stock1.price = priceData1;
+
+          price2
+            .then(priceData2 => {
+              const stock2 = {};
+              stock2.stock = stock[1];
+              stock2.price = priceData2;
+
+              likes1
+                .then(likeData1 => {
+                  likes2
+                    .then(likeData2 => {
+                      stock1.rel_likes = likeData1.likes - likeData2.likes;
+                      stock2.rel_likes = likeData2.likes - likeData1.likes;
+
+                      stockData.push(stock1);
+                      stockData.push(stock2);
+
+                      res.json({ stockData });
+                    })
+                    .catch(err => console.log(err));
+                })
+                .catch(err => console.log(err));
+            })
+            .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
+    } else {
+      // regular process for one stock only
+      const stockData = { stock: stock };
+      const price = stockPrices.getPrice(stock);
+      const likes = stockPrices.handleLikes(stock, like, ip);
+
+      price
+        .then(priceData => {
+          stockData.price = priceData;
+
+          likes
+            .then(likeData => {
+              stockData.likes = likeData.likes;
+              res.json({ stockData });
+            })
+            .catch(err => console.log(err));
+        })
+        .catch(err => console.log(err));
+    }
   });
 };
